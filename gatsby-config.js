91ce8ecf09fb94e14622bb9d18816f151b5e8826
monichre <liamhellis@gmail.com>
2020-02-22 00:04:31 -0500

    env variables for fucking gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ if (process.env.ENVIROMENT !== 'production') {
 }
 
 const contentfulConfig = {
-  spaceId: process.env.SPACE_ID,
-  accessToken: process.env.ACCESS_TOKEN,
+  spaceId: process.env.GATSBY_SPACE_ID_SPACE_ID,
+  accessToken: process.env.GATSBY_ACCESS_TOKEN,
 }
 
 module.exports = {
